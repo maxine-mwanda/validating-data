c546feab2ae448087ce2a8e070a0f9dadf33423a
Maxine <mwandamaxyine@gmail.com>
2020-08-21 11:06:32 +0300

    data validation

--- main.go
+++ main.go
@@ -1,45 +1,44 @@
 package main
 
 import (
+	"errors"
 	_ "errors"
 )
 
-func main () {}
+func main() {}
 
-	type Journalist struct {
-		Name            string `json:"name"`
-		Gender          string `json:"gender"`
-		Age             int    `json:"age"`
-		EmployeeNumber int    `json:"employee_number"`
-		IdNumber       int    `json:"id_number"`
-	}
+type Journalist struct {
+	Name           string `json:"name"`
+	Gender         string `json:"gender"`
+	Age            int    `json:"age"`
+	EmployeeNumber int    `json:"employee_number"`
+	IdNumber       int    `json:"id_number"`
+}
 
-	type Journalist2 struct {
-		Name            string `json:"name"`
-		Gender          string `json:"gender"`
-		Age             int `json:"age"`
-		EmployeeNumber int `json:"employee_number"`
-		IdNumber       int `json:"id_number"`
-	}
+type Journalist2 struct {
+	Name           string `json:"name"`
+	Gender         string `json:"gender"`
+	Age            int    `json:"age"`
+	EmployeeNumber int    `json:"employee_number"`
+	IdNumber       int    `json:"id_number"`
+}
 
-	func(J *Journalist) Validate() {
-	}
-
-		if J.Name == ""{
+func (J *Journalist) Validate() error {
+	if J.Name == "" {
 		return errors.New("Name is required")
 	}
-		if J.Age > 19{
+	if J.Age > 19 {
 		return errors.New("Welcome")
 	}
 
-		if J.Gender == ""{
+	if J.Gender == "" {
 		return errors.New("Gender is required")
 	}
-		if J.Employee_number < 0{
+	if J.EmployeeNumber < 0 {
 		return errors.New("Insert employee number")
 	}
-		if J.Id_number < 0{
+	if J.IdNumber < 0 {
 		return errors.New("Insert valid Id")
 	}
-		return nil
-	}
+	return nil
+}
